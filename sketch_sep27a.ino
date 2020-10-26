93c99cc083775ce2ec9a8f539daf8caba1f328c4
askl011 <55929169+askl011@users.noreply.github.com>
2020-10-26 12:11:30 +0300

    Update sketch_sep27a.ino

--- sketch_sep27a.ino
+++ sketch_sep27a.ino
@@ -1,22 +1,4 @@
-/*******************************************************************
-    A telegram bot for your ESP8266 that responds
-    with whatever message you send it.
 
-    Parts:
-    D1 Mini ESP8266 * - http://s.click.aliexpress.com/e/uzFUnIe
-    (or any ESP8266 board)
-
-      = Affilate
-
-    If you find what I do useful and would like to support me,
-    please consider becoming a sponsor on Github
-    https://github.com/sponsors/witnessmenow/
-
-
-    Written by Brian Lough
-    YouTube: https://www.youtube.com/brianlough
-    Tindie: https://www.tindie.com/stores/brianlough/
-    Twitter: https://twitter.com/witnessmenow
  *******************************************************************/
 
 // NOTE: The version of ESP8266 core needs to be 2.5 or higher
